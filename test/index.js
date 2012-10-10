56719a90415fb900427c5a2536da63e51454b395
Craig McDonald <github.com@thrackle.com>
2012-10-10 09:25:40 -0600

    first!

---
+++ test/index.js
@@ -0,0 +1,66 @@
+var express = require("express")
+  , request = require("superagent")
+  , zip = require("../")
+  , app = express.createServer()
+  , expect = require("chai").expect;
+
+app.get("/test/1", function(req, res) {
+  res.zip([
+    { path: __dirname + "/zip_contents/data1.txt", name: "data1.txt" },
+    { path: __dirname + "/zip_contents/data2.txt", name: "/zip_contents/data2.txt" }
+  ]);
+});
+
+app.get("/test/2", function(req, res) {
+  res.zip([
+    { path: __dirname + "/zip_contents/data1.txt", name: "data1.txt" },
+    { path: __dirname + "/zip_contents/data2.txt", name: "/zip_contents/data2.txt" }
+  ], "test2.zip");
+});
+
+app.listen(8383);
+
+describe('express-zip', function() {
+  it('should expose .options', function() {
+    expect(zip.options).to.not.equal(undefined);
+  });
+
+  it('should extend res.zip', function() {
+    if (express.version.match(/^2\.[0-9]+\.[0-9]+$/)) {
+      // express 2.x
+      expect(require('http').ServerResponse.prototype.zip).to.be.a('function');
+    } else {
+      // express 3.x
+      expect(require('express').response.zip).to.be.a('function');
+    }
+  });
+});
+
+describe('res.zip()', function() {
+  it('should response valid content-type', function(done) {
+    request
+      .get('http://127.0.0.1:8383/test/1')
+      .end(function(res) {
+        expect(res.headers['content-type']).to.match(/^application\/zip/);
+        done();
+      });
+  });
+
+  it('should response valid content-disposition', function(done) {
+    request
+      .get('http://127.0.0.1:8383/test/1')
+      .end(function(res) {
+        expect(res.headers['content-disposition']).to.match(/^attachment; filename="attachment.zip"/);
+        done();
+      });
+  });
+
+  it('can pass filename', function(done) {
+    request
+      .get('http://127.0.0.1:8383/test/2')
+      .end(function(res) {
+        expect(res.headers['content-disposition']).to.match(/^attachment; filename="test2.zip"/);
+        done();
+      });
+  });
+});
